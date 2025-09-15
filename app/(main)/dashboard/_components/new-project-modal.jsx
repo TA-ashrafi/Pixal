9c4a723beedf9da32ab52e6cc77c08e6229bb006
Tahseen Ashrafi <theperfectgamer100@gmail.com>
2025-09-16 01:53:01 +0530

    backend for Free and Pro User

--- app/(main)/dashboard/_components/new-project-modal.jsx
+++ app/(main)/dashboard/_components/new-project-modal.jsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import {
   Dialog,
   DialogContent,
@@ -7,24 +8,38 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-
+import { api } from "@/convex/_generated/api"
+import { useConvexQuery } from "@/hooks/use-convex-query";
+import { usePlanAccess } from "@/hooks/use-plan-access";
 import React from 'react'
 
 const NewProjectModal = ({ isOpen , onClose}) => {
 const handelClose = () => {
     onClose();
-}
+};
+
+const { data : projects } = useConvexQuery(api.projects.getUserProjects);
+const currentProjectCount = projects?.length || 0; 
+
+const {isFree , canCreateProject} = usePlanAccess();
+const canCreate = canCreateProject(currentProjectCount);
 
   return (
     <>
     <Dialog open={isOpen} onOpenChange = {handelClose}>
   <DialogContent>
     <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
+      <DialogTitle className="text-2xl font-bbold text-white">Create New Project</DialogTitle>
+      {/* <DialogDescription>
         This action cannot be undone. This will permanently delete your account
         and remove your data from our servers.
-      </DialogDescription>
+      </DialogDescription> */}
+
+      {isFree && (
+        <Badge variant="secondary" className="bg-slate-700 text-white/70">
+            {currentProjectCount}/3 Projects
+        </Badge>
+      )}
     </DialogHeader>
 
     <DialogFooter>Footer</DialogFooter>
